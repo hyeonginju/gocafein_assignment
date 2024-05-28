a2f79aa99856d8220a107d23f16e3bd0d9bb4e5c
hyeonginju <guddls64@naver.com>
2024-05-28 23:46:51 +0900

    🎨 영화 상세 페이지 데이터 연결 repo

---
+++ lib/logic/repositories/movie/movie_detail_repo.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'package:gocafein/tools/config.dart';
+import 'package:http/http.dart' as http;
+
+class MovieDetailRepo {
+  Future<Map<String, dynamic>> getMovieDetailData(
+      {required String imdbID}) async {
+    Map<String, dynamic> datas = {};
+
+    final response = await http.get(
+      Uri.parse(
+          'https://www.omdbapi.com/?apikey=${Config.openMovieApiKey}&i=$imdbID'),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+
+    final int statusCode = response.statusCode;
+    final responseData = json.decode(response.body);
+
+    if (statusCode != 200) {
+      datas["errorCode"] = statusCode;
+      datas["data"] = responseData ?? {};
+      return datas;
+    }
+
+    datas["data"] = responseData ?? {};
+
+    return datas;
+  }
+}
