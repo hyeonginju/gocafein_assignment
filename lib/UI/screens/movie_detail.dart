759d7b64e1d58aba46aa93678246400e378ee432
hyeonginju <guddls64@naver.com>
2024-05-28 23:47:42 +0900

    🎨 영화 상세 페이지 데이터 연결 작업 및 UI 수정

--- lib/UI/screens/movie_detail.dart
+++ lib/UI/screens/movie_detail.dart
@@ -1,15 +1,19 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gocafein/logic/blocs/movie_detail/movie_detail_bloc.dart';
+import 'package:gocafein/logic/blocs/movie_detail/movie_detail_event.dart';
+import 'package:gocafein/logic/blocs/movie_detail/movie_detail_state.dart';
+import 'package:gocafein/logic/models/movie/movie_model.dart';
+import 'package:gocafein/logic/models/movieDetail/movie_detail_model.dart';
 import 'package:gocafein/tools/global_variable.dart';
+import 'package:gocafein/tools/tools.dart';
 
 class MovieDetailScreen extends StatefulWidget {
-  final String movieId;
-
+  final MovieModel movie;
   const MovieDetailScreen({
-    Key? key,
-    required this.movieId,
-  }) : super(key: key);
+    super.key,
+    required this.movie,
+  });
 
   @override
   State<MovieDetailScreen> createState() => _MovieDetailScreenState();
@@ -20,17 +24,32 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     final double screenWidth = size.width;
-    final double imageWidth = screenWidth * 0.65;
+    final double screenHeight = size.height;
+    final String imageUrl = widget.movie.Poster.isNotEmpty &&
+            (widget.movie.Poster.startsWith('http') ||
+                widget.movie.Poster.startsWith('https'))
+        ? widget.movie.Poster
+        : 'https://via.placeholder.com/300x450.png?text=No+Image';
 
     TextStyle whiteBold16 = const TextStyle(
       color: GlobalVariable.whiteColor,
       fontWeight: FontWeight.bold,
       fontSize: 16,
+      height: 24 / 16,
+    );
+
+    TextStyle titleTextStyle = const TextStyle(
+      color: GlobalVariable.whiteColor,
+      fontWeight: FontWeight.w800,
+      fontSize: 20,
+      height: 24 / 20,
     );
 
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        backgroundColor: GlobalVariable.mainColor,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
         leading: IconButton(
           icon: const Icon(Icons.arrow_back, color: GlobalVariable.whiteColor),
           onPressed: () {
@@ -39,98 +58,166 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
         ),
       ),
       body: Container(
-        padding: const EdgeInsets.only(bottom: 20),
         color: GlobalVariable.mainColor,
-        child: Column(
-          children: [
-            Expanded(
-              child: Stack(
-                children: [
-                  Positioned(
-                    top: 0,
-                    left: 0,
-                    child: ShaderMask(
-                      shaderCallback: (rect) {
-                        return LinearGradient(
-                          begin: Alignment.centerLeft,
-                          end: Alignment.centerRight,
-                          colors: [
-                            Colors.white,
-                            Colors.white.withOpacity(0.0),
-                          ],
-                          stops: [0.4, 1.0],
-                        ).createShader(rect);
-                      },
-                      blendMode: BlendMode.dstIn,
-                      child: ClipRRect(
-                        child: Image.asset(
-                          'assets/images/logos/movie_poster_sample.png',
-                          width: imageWidth,
-                          fit: BoxFit.cover,
-                        ),
+        height: screenHeight,
+        child: SingleChildScrollView(
+            padding: const EdgeInsets.only(bottom: 80),
+            child: BlocBuilder<MovieDetailBloc, MovieDetailState>(
+                builder: (context, state) {
+              if (state is MovieDetailInitial) {
+                context.read<MovieDetailBloc>().add(
+                      IsMovieDetailEvent(
+                        imdbID: widget.movie.imdbID,
                       ),
-                    ),
+                    );
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              if (state is MovieDetailLoading) {
+                return SizedBox(
+                  height: screenHeight,
+                  child: const Center(
+                    child: CircularProgressIndicator(),
                   ),
-                  Positioned(
-                    top: 0,
-                    left: screenWidth * 0.65 - imageWidth * 0.2 + 10,
-                    child: Container(
-                      width: screenWidth * 0.35 + imageWidth * 0.2 - 10,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                );
+              }
+              if (state is MovieDetailSuccess) {
+                MovieDetailModel detailData =
+                    state.detailData as MovieDetailModel;
+                String rottenTomatoesValue = '';
+                List<dynamic> ratings = detailData.Ratings;
+                for (var rating in ratings) {
+                  if (rating['Source'] == 'Rotten Tomatoes') {
+                    rottenTomatoesValue = rating['Value'];
+                    break;
+                  }
+                }
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SizedBox(
+                      height: screenWidth * 1.5, // 이미지 높이 고정
+                      child: Stack(
                         children: [
-                          // const SizedBox(height: 30),
-                          Text(
-                            'Boy Hood 12312 123 123123',
-                            style:
-                                whiteBold16.copyWith(fontSize: 20, height: 1.1),
-                            overflow: TextOverflow.visible,
-                            softWrap: true,
+                          Positioned(
+                            top: 0,
+                            left: 0,
+                            right: 0,
+                            bottom: 0,
+                            child: ShaderMask(
+                              shaderCallback: (rect) {
+                                return LinearGradient(
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                  colors: [
+                                    Colors.white,
+                                    Colors.white.withOpacity(0.0),
+                                  ],
+                                  stops: const [0.65, 1],
+                                ).createShader(rect);
+                              },
+                              blendMode: BlendMode.dstIn,
+                              child: ClipRRect(
+                                child: Image.network(
+                                  imageUrl,
+                                  width: screenWidth,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
                           ),
-                          const SizedBox(height: 15),
-                          Text(
-                            '1977.05.25',
-                            style: whiteBold16,
-                            overflow: TextOverflow.visible,
-                            softWrap: true,
+                          Positioned(
+                            bottom: 10,
+                            left: 0,
+                            right: 0,
+                            child: Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: screenWidth * 0.1),
+                              width: screenWidth,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  RichText(
+                                    textAlign: TextAlign.center,
+                                    text: TextSpan(
+                                      style: titleTextStyle,
+                                      children: widget.movie.Title
+                                          .split('')
+                                          .map((char) => TextSpan(text: char))
+                                          .toList(),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Text(
+                                    Tools.convertReleased(detailData.Released),
+                                    style: whiteBold16,
+                                    overflow: TextOverflow.visible,
+                                    softWrap: true,
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Text(
+                                    Tools.convertRuntime(detailData.Runtime),
+                                    style: whiteBold16,
+                                    overflow: TextOverflow.visible,
+                                    softWrap: true,
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Text(
+                                    Tools.convertBoxOffice(
+                                        detailData.BoxOffice),
+                                    style: whiteBold16,
+                                    overflow: TextOverflow.visible,
+                                    softWrap: true,
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Text(
+                                    '🍅 $rottenTomatoesValue',
+                                    style: whiteBold16,
+                                    overflow: TextOverflow.visible,
+                                    softWrap: true,
+                                  ),
+                                ],
+                              ),
+                            ),
                           ),
-                          const SizedBox(height: 10),
-                          Text(
-                            '121분',
-                            style: whiteBold16,
-                            overflow: TextOverflow.visible,
-                            softWrap: true,
-                          ),
-                          const SizedBox(height: 10),
-                          Text(
-                            '1300만명',
-                            style: whiteBold16,
-                            overflow: TextOverflow.visible,
-                            softWrap: true,
-                          ),
-                          const SizedBox(height: 10),
-                          Text(
-                            '🍅 95%',
-                            style: whiteBold16,
-                            overflow: TextOverflow.visible,
-                            softWrap: true,
-                          ),
-                          const SizedBox(height: 10),
-                          // Text(
-                          //   '⭐️ 8.6',
-                          //   style: whiteBold16,
-                          //   overflow: TextOverflow.visible,
-                          //   softWrap: true,
-                          // ),
                         ],
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+                    const SizedBox(height: 10),
+                    if (detailData.Plot == "N/A") ...[
+                      Text('줄거리 정보가 없습니다.', style: whiteBold16),
+                    ] else ...[
+                      SizedBox(
+                        width: screenWidth * 0.8,
+                        child: Row(
+                          children: [
+                            Flexible(
+                              child: RichText(
+                                maxLines: null,
+                                textAlign: TextAlign.center,
+                                text: TextSpan(
+                                  style: whiteBold16,
+                                  children:
+                                      detailData.Plot.split('').map((char) {
+                                    return TextSpan(
+                                      text: char,
+                                      style: whiteBold16,
+                                    );
+                                  }).toList(),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ]
+                  ],
+                );
+              } else {
+                return const SizedBox.shrink();
+              }
+            })),
       ),
     );
   }
--- lib/tools/tools.dart
+++ lib/tools/tools.dart
@@ -1,1 +1,56 @@
-class Tools {}
+class Tools {
+  static String convertBoxOffice(String boxOffice) {
+    int amount =
+        int.tryParse(boxOffice.replaceAll(r'$', '').replaceAll(',', '')) ?? 0;
+    double million = amount / 10000;
+
+    // 만명 미만인 경우 '약 1만명 이하'로 표시
+    if (million < 1) {
+      return '1만명 이하';
+    }
+    double billion = million / 10000;
+    // 억이 넘어가는 경우
+    if (billion >= 1) {
+      return '${billion.toStringAsFixed(1)}억명';
+    }
+
+    // 그 외의 경우
+    return '${million.toInt()}만 명';
+  }
+
+  static String convertRuntime(String runtime) {
+    int minutes = int.tryParse(runtime.replaceAll(' min', '')) ?? 0;
+    return '$minutes \분';
+  }
+
+  static String convertReleased(String released) {
+    if (released == "N/A") {
+      return '개봉일 정보 없음';
+    }
+    List<String> parts = released.split(' ');
+    int day = int.tryParse(parts[0]) ?? 0;
+    String month = parts[1];
+    int year = int.tryParse(parts[2]) ?? 0;
+
+    Map<String, String> monthMap = {
+      'Jan': '01',
+      'Feb': '02',
+      'Mar': '03',
+      'Apr': '04',
+      'May': '05',
+      'Jun': '06',
+      'Jul': '07',
+      'Aug': '08',
+      'Sep': '09',
+      'Oct': '10',
+      'Nov': '11',
+      'Dec': '12'
+    };
+
+    String monthNumber = monthMap[month] ?? '01'; // 월을 숫자로 변환
+    String dayString = day.toString().padLeft(2, '0'); // 일을 두 자리 숫자로 변환
+    String formattedDate = '$year.$monthNumber.$dayString';
+
+    return formattedDate;
+  }
+}
